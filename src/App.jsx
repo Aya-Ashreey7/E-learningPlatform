c2926f533bf589382082248c8cf6e7acdec33720
mariem-mourad <mariemmouraad@gmail.com>
2025-08-12 03:55:46 +0300

    add the layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-
 import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import './index.css'
@@ -8,13 +7,16 @@ import { Toaster } from 'react-hot-toast'
 import VerifiedRedirect from './components/VerifiedRedirect'
 import VerifyEmail from './components/VerifyEmail'
 import Home from './pages/Home'
-
+import Hero from './components/Hero/Hero' // Import Hero
+import AboutUs from './pages/AboutUs' // Import AboutUs page
 function App() {
 
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Hero />} /> {/* Use Hero as the home page */}
+        <Route path="/aboutus" element={<AboutUs />} />
+
         <Route path="/login" element={<LoginForm />} />
         <Route path="/register" element={<RegisterForm />} />
         <Route path="/verify-email" element={<VerifyEmail />} />
