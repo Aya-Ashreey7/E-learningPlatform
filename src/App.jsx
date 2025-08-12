2de011d989b9e2d2b24c2266c85641a542ca1797
Aya Ashreey <Ayaashry68@gmail.com>
2025-08-12 18:16:19 +0300

    update index.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,7 @@
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import "./index.css";
+import { Toaster } from "react-hot-toast";
 
 
 import CoursesPage from './pages/CoursesPage';
@@ -26,8 +30,6 @@ import PaymentSuccess from './components/Checkout/PaymentSuccess';
 import Orders from './pages/orders';
 import ResetPassword from './pages/ResetPassword';
 import Feedback from './pages/Feedback';
-import { Route, Routes } from 'react-router-dom';
-import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
@@ -46,7 +48,7 @@ function App() {
           <Route path="/forgot-password" element={<ResetPassword />} />
 
 
-         {/* Dashboard */}
+          {/* Dashboard */}
           <Route path="/dashboard/overview" element={<Overview />} />
           <Route path="/dashboard/add-course" element={<AddCourse />} />
           <Route path="/dashboard/manage-courses" element={<ManageCourses />} />
@@ -59,7 +61,7 @@ function App() {
           <Route path="/checkout/checkout" element={<Checkout />} />
           <Route path="/checkout/payment" element={<PaymentModal />} />
           <Route path="/paymentSuccess" element={<PaymentSuccess />} />
-            
+
         </Routes>
         <Toaster position="top-center" reverseOrder={false} />
       </CartProvider>
