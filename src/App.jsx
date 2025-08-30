53dbbed9a31e05ae63a2bc0ee75a6754e70f23fc
monicahanna1942 <monicahanna1942@gmail.com>
2025-08-30 04:58:43 -0700

    ProtectedRoute.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import {  Routes, Route } from "react-router-dom";
+// App.jsx
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import "./App.css";
 import "./index.css";
 import { Toaster } from "react-hot-toast";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 // Pages & Components
 import Hero from "./components/Hero/Hero";
-import AboutUs from "./pages/AboutUs";
+import AboutUs from "./pages/AboutUs";  
 import CoursesPage from "./pages/CoursesPage";
 import CartPage from "./pages/CartPage";
 import ContactPage from "./pages/ContactPage";
@@ -20,7 +22,6 @@ import Users from "./pages/Users";
 import Orders from "./pages/orders";
 import ResetPassword from "./pages/ResetPassword";
 import Feedback from "./pages/Feedback";
-
 import ProfilePage from "./pages/ProfilePage";
 import EditProfile from "./pages/EditProfile";
 import NotificationsPage from "./pages/NotificationsPage";
@@ -37,49 +38,146 @@ import Wishlist from "./pages/Wishlist";
 import CourseAdults from "./pages/Courseadults";
 import CourseDetails from "./pages/CourseDetails";
 
-
 function App() {
   return (
     <CartProvider>
-      <Routes>
-        {/* Public Pages */}
-        <Route path="/" element={<Hero />} />
-        <Route path="/wishlist" element={<Wishlist />} />
-        <Route path="/aboutus" element={<AboutUs />} />
-        <Route path="/courses" element={<CoursesPage />} />
-        <Route path="/courses/kids" element={<CourseKids />} />
-        <Route path="/courses/adults" element={<CourseAdults />} />
-        <Route path="/course/:id" element={<CourseDetails />} />
-        <Route path="/cart" element={<CartPage />} />
-        <Route path="/contact" element={<ContactPage />} />
-        <Route path="/login" element={<LoginForm />} />
-        <Route path="/register" element={<RegisterForm />} />
-        <Route path="/verify-email" element={<VerifyEmail />} />
-        <Route path="/verified-redirect" element={<VerifiedRedirect />} />
-        <Route path="/forgot-password" element={<ResetPassword />} />
-        <Route path="/courses/kids/:id" element={<KidsCourseDetails />} />
+      
+        <Routes>
+          {/* Public Pages */}
+          <Route path="/" element={<Hero />} />
+          <Route path="/wishlist" element={<Wishlist />} />
+          <Route path="/aboutus" element={<AboutUs />} />
+          <Route path="/courses" element={<CoursesPage />} />
+          <Route path="/courses/kids" element={<CourseKids />} />
+          <Route path="/courses/adults" element={<CourseAdults />} />
+          <Route path="/course/:id" element={<CourseDetails />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/register" element={<RegisterForm />} />
+          <Route path="/verify-email" element={<VerifyEmail />} />
+          <Route path="/verified-redirect" element={<VerifiedRedirect />} />
+          <Route path="/forgot-password" element={<ResetPassword />} />
+          <Route path="/courses/kids/:id" element={<KidsCourseDetails />} />
 
-        {/* Dashboard */}
-        <Route path="/dashboard/overview" element={<Overview />} />
-        <Route path="/dashboard/add-course" element={<AddCourse />} />
-        <Route path="/dashboard/manage-courses" element={<ManageCourses />} />
-        <Route path="/dashboard/users" element={<Users />} />
-        <Route path="/dashboard/orders" element={<Orders />} />
-        <Route path="/dashboard/feedback" element={<Feedback />} />
+          {/* Protected Pages */}
+          <Route
+            path="/Cart"
+            element={
+              <ProtectedRoute>
+                <CartPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/Notifications"
+            element={
+              <ProtectedRoute>
+                <NotificationsPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/ProfilePage"
+            element={
+              <ProtectedRoute>
+                <ProfilePage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/edit-profile"
+            element={
+              <ProtectedRoute>
+                <EditProfile />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/checkout/address"
+            element={
+              <ProtectedRoute>
+                <AddressForm />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/checkout/checkout"
+            element={
+              <ProtectedRoute>
+                <Checkout />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/checkout/payment"
+            element={
+              <ProtectedRoute>
+                <PaymentModal />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/paymentSuccess"
+            element={
+              <ProtectedRoute>
+                <PaymentSuccess />
+              </ProtectedRoute>
+            }
+          />
 
-        {/* Checkout */}
-        <Route path="/checkout/address" element={<AddressForm />} />
-        <Route path="/checkout/checkout" element={<Checkout />} />
-        <Route path="/checkout/payment" element={<PaymentModal />} />
-        <Route path="/paymentSuccess" element={<PaymentSuccess />} />
+          {/* Admin Dashboard Pages */}
+          <Route
+            path="/dashboard/overview"
+            element={
+              <ProtectedRoute adminOnly={true}>
+                <Overview />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/dashboard/add-course"
+            element={
+              <ProtectedRoute adminOnly={true}>
+                <AddCourse />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/dashboard/manage-courses"
+            element={
+              <ProtectedRoute adminOnly={true}>
+                <ManageCourses />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/dashboard/users"
+            element={
+              <ProtectedRoute adminOnly={true}>
+                <Users />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/dashboard/orders"
+            element={
+              <ProtectedRoute adminOnly={true}>
+                <Orders />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/dashboard/feedback"
+            element={
+              <ProtectedRoute adminOnly={true}>
+                <Feedback />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
 
-        {/* User Profile */}
-        <Route path="/ProfilePage" element={<ProfilePage />} />
-        <Route path="/edit-profile" element={<EditProfile />} />
-        <Route path="/notifications" element={<NotificationsPage />} />
-      </Routes>
-
-      <Toaster position="top-center" reverseOrder={false} />
+        <Toaster position="top-center" reverseOrder={false} />
+     
     </CartProvider>
   );
 }
