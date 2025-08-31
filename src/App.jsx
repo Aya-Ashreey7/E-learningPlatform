ccc8b144bc61394a683408cb9a730ba6d6f3c9f9
hedra talat <hedratalat1717@gmail.com>
2025-08-31 11:16:04 +0300

    save

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,6 @@ import CourseDetails from "./pages/CourseDetails";
 import BlogDashboard from "./pages/blogDashboard";
 import BlogPage from "./pages/blogUser";
 
-
 function App() {
   return (
     <CartProvider>
@@ -52,7 +51,15 @@ function App() {
         <Route path="/courses/kids" element={<CourseKids />} />
         <Route path="/courses/adults" element={<CourseAdults />} />
         <Route path="/course/:id" element={<CourseDetails />} />
-        <Route path="/Cart" element={<ProtectedRoute> <CartPage /> </ProtectedRoute>} />
+        <Route
+          path="/Cart"
+          element={
+            <ProtectedRoute>
+              {" "}
+              <CartPage />{" "}
+            </ProtectedRoute>
+          }
+        />
         <Route path="/contact" element={<ContactPage />} />
         <Route path="/login" element={<LoginForm />} />
         <Route path="/register" element={<RegisterForm />} />
@@ -62,28 +69,144 @@ function App() {
         <Route path="/courses/kids/:id" element={<KidsCourseDetails />} />
         <Route path="/blog" element={<BlogPage />} />
 
-
         {/* Dashboard protected*/}
-        <Route path="/dashboard/overview" element={<ProtectedRoute adminOnly={true}> <Overview /> </ProtectedRoute>} />
-        <Route path="/dashboard/overview" element={<ProtectedRoute adminOnly={true}> <Overview /> </ProtectedRoute>} />
-        <Route path="/dashboard/add-course" element={<ProtectedRoute adminOnly={true}> <AddCourse /> </ProtectedRoute>} />
-        <Route path="/dashboard/manage-courses" element={<ProtectedRoute adminOnly={true}> <ManageCourses /> </ProtectedRoute>} />
-        <Route path="/dashboard/users" element={<ProtectedRoute adminOnly={true}> <Users /> </ProtectedRoute>} />
-        <Route path="/dashboard/orders" element={<ProtectedRoute adminOnly={true}> <Orders /> </ProtectedRoute>} />
-        <Route path="/dashboard/feedback" element={<ProtectedRoute adminOnly={true}> <Feedback /> </ProtectedRoute>} />
-        <Route path="/dashboard/addblog" element={<ProtectedRoute adminOnly={true}> <BlogDashboard /> </ProtectedRoute>} />
-
+        <Route
+          path="/dashboard/overview"
+          element={
+            <ProtectedRoute adminOnly={true}>
+              {" "}
+              <Overview />{" "}
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/dashboard/overview"
+          element={
+            <ProtectedRoute adminOnly={true}>
+              {" "}
+              <Overview />{" "}
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/dashboard/add-course"
+          element={
+            <ProtectedRoute adminOnly={true}>
+              {" "}
+              <AddCourse />{" "}
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/dashboard/manage-courses"
+          element={
+            <ProtectedRoute adminOnly={true}>
+              {" "}
+              <ManageCourses />{" "}
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/dashboard/users"
+          element={
+            <ProtectedRoute adminOnly={true}>
+              {" "}
+              <Users />{" "}
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/dashboard/orders"
+          element={
+            <ProtectedRoute adminOnly={true}>
+              {" "}
+              <Orders />{" "}
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/dashboard/feedback"
+          element={
+            <ProtectedRoute adminOnly={true}>
+              {" "}
+              <Feedback />{" "}
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/dashboard/addblog"
+          element={
+            <ProtectedRoute adminOnly={true}>
+              {" "}
+              <BlogDashboard />{" "}
+            </ProtectedRoute>
+          }
+        />
 
         {/* Checkout protected*/}
-        <Route path="/checkout/address" element={<ProtectedRoute><AddressForm />  </ProtectedRoute>} />
-        <Route path="/checkout/checkout" element={<ProtectedRoute> <Checkout /> </ProtectedRoute>} />
-        <Route path="/checkout/payment" element={<ProtectedRoute> <PaymentModal /> </ProtectedRoute>} />
-        <Route path="/paymentSuccess" element={<ProtectedRoute> <PaymentSuccess /> </ProtectedRoute>} />
+        <Route
+          path="/checkout/address"
+          element={
+            <ProtectedRoute>
+              <AddressForm />{" "}
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/checkout/checkout"
+          element={
+            <ProtectedRoute>
+              {" "}
+              <Checkout />{" "}
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/checkout/payment"
+          element={
+            <ProtectedRoute>
+              {" "}
+              <PaymentModal />{" "}
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/paymentSuccess"
+          element={
+            <ProtectedRoute>
+              {" "}
+              <PaymentSuccess />{" "}
+            </ProtectedRoute>
+          }
+        />
 
         {/* User Profile protected*/}
-        <Route path="/Notifications" element={<ProtectedRoute><NotificationsPage /> </ProtectedRoute>} />
-        <Route path="/ProfilePage" element={<ProtectedRoute> <ProfilePage /></ProtectedRoute>} />
-        <Route path="/edit-profile" element={<ProtectedRoute> <EditProfile /> </ProtectedRoute>} />
+        <Route
+          path="/Notifications"
+          element={
+            <ProtectedRoute>
+              <NotificationsPage />{" "}
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/ProfilePage"
+          element={
+            <ProtectedRoute>
+              {" "}
+              <ProfilePage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/edit-profile"
+          element={
+            <ProtectedRoute>
+              {" "}
+              <EditProfile />{" "}
+            </ProtectedRoute>
+          }
+        />
       </Routes>
 
       <Toaster position="top-center" reverseOrder={false} />
